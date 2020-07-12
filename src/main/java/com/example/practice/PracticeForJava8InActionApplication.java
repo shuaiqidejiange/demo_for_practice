a1693497d3945ffb3199f125d66d2400e8c1649a
wangjian <1171155814@qq.com>
2020-07-12 17:17:15 +0800

    init

---
+++ src/main/java/com/example/practice/PracticeForJava8InActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeForJava8InActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeForJava8InActionApplication.class, args);
+	}
+
+}
